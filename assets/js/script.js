72f3e4ff6de21b4265d0345c9b1903a6f50fdc96
Ana Luisa <anadrvv@gmail.com>
2025-05-01 11:11:24 -0300

    page

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const result = document.getElementById("result");
+const image = document.getElementById("image");
+const colorDiv = document.querySelector(".colorDiv");
+const colorEnterText = document.querySelector(".color-text");
+
+button.addEventListener("click", () => {
+  const originalText = input.value;
+
+  if (!originalText) {
+    alert("enter a value");
+    return;
+  }
+  
+  const cleanText = originalText.toLowerCase().replace(/[^a-z0-9]/gi, "");
+  const reverseText = cleanText.split("").reverse().join("");
+  const isPalindrome = cleanText === reverseText;
+
+  result.classList.remove("initial-text");
+  result.textContent = `${originalText} is ${
+    isPalindrome ? "" : "not "
+  }a palindrome`;
+
+  image.style.backgroundImage = isPalindrome
+    ? 'url("assets/images/is.jpg")'
+    : 'url("assets/images/not.jpg")';
+
+  colorDiv.style.backgroundColor = isPalindrome ? "#083D5C" : "#1C2A47";
+  result.style.color = isPalindrome ? "#083D5C" : "#1C2A47";
+  colorEnterText.style.color = isPalindrome ? "#083D5C" : "#1C2A47";
+
+  button.classList.remove("btn-palindrome", "btn-not-palindrome");
+  button.classList.add(isPalindrome ? "btn-palindrome" : "btn-not-palindrome");
+});
+
